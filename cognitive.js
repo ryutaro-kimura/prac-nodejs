e0933ce03f868471579b6cfc4226b09d41d7976e
ryutaro-kimura <sasuker2264@gmail.com>
2022-02-24 00:06:02 +0900

    rename: ファイル名の変更

--- cognitive.js
+++ cognitive.js
@@ -1,18 +1,16 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
-var URL = 'https://praccustomvisionryutaro-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/ddf1b811-6328-4d08-b51b-c7e0e1da27d0/classify/iterations/Iteration1/image';
-
-request.post({
-    uri: URL,
+const cogUrl = 'https://praccustomvisionryutaro-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/ddf1b811-6328-4d08-b51b-c7e0e1da27d0/classify/iterations/Iteration1/image';
+const options = {
+    method: 'GET',
     headers: { 
         "Prediction-Key": "103c3c4ff51340eeade8e614f0aed4bf",
         "Content-type": "application/json"
     },
-    body: {
-        "Url": "https://stgphotostorage.blob.core.windows.net/mocks/candidate-list.png"
-    },
-}, (err, res, data) => {
-    console.log(data);
-});
+};
 
-// 参考：https://qiita.com/yuta0801/items/ff7f314f45c4f8dc8a48
+const res = fetch(cogUrl, options)
+.then(data => {
+    // console.log(data); // `data.json()` の呼び出しで解釈された JSON データ
+  })
+console.log(res);
